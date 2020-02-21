66b1193a69a95b5e62861e459cfed5fee878c5d7
Jacob Beard <jake@minnow.io>
2020-02-21 06:35:14 -0500

    Reformat

--- lacecore/_obj.py
+++ lacecore/_obj.py
@@ -4,11 +4,14 @@ from ._group_map import GroupMap
 from collections import OrderedDict
 from tinyobjloader import ObjReader, ObjReaderConfig
 
+
 class LoadException(Exception):
     pass
 
+
 class ArityException(Exception):
     pass
+
 
 def load(mesh_path, triangulate=False):
     """
@@ -23,29 +26,37 @@ def load(mesh_path, triangulate=False):
     """
     reader = ObjReader()
     config = ObjReaderConfig()
-    config.triangulate = triangulate 
+    config.triangulate = triangulate
     success = reader.ParseFromFile(mesh_path, config)
     if success == False:
         raise LoadException(reader.Warning() or reader.Error())
     attrib = reader.GetAttrib()
     shapes = reader.GetShapes()
     tinyobj_vertices = attrib.numpy_vertices().reshape(-1, 3)
-    all_vertices_per_face = np.concatenate([ shape.mesh.numpy_num_face_vertices() for shape in shapes ])
+    all_vertices_per_face = np.concatenate(
+        [shape.mesh.numpy_num_face_vertices() for shape in shapes]
+    )
     first_arity = all_vertices_per_face[0]
-    if np.any(all_vertices_per_face != first_arity) or np.any(all_vertices_per_face > 4) or np.any(all_vertices_per_face < 3):
-        raise ArityException('OBJ Loader does not support mixed arities, or arities greater than 4 or less than 3')
+    if (
+        np.any(all_vertices_per_face != first_arity)
+        or np.any(all_vertices_per_face > 4)
+        or np.any(all_vertices_per_face < 3)
+    ):
+        raise ArityException(
+            "OBJ Loader does not support mixed arities, or arities greater than 4 or less than 3"
+        )
 
     segm = OrderedDict()
     all_faces = None
 
     for shape in shapes:
-        tinyobj_all_indices = shape.mesh.numpy_indices().reshape(-1, 3)[:,0]
+        tinyobj_all_indices = shape.mesh.numpy_indices().reshape(-1, 3)[:, 0]
         faces = tinyobj_all_indices.reshape(-1, first_arity)
         start = len(all_faces) if all_faces is not None else 0
         end = start + len(faces)
         all_faces = faces if all_faces is None else np.concatenate((all_faces, faces))
 
-        group_names = shape.name.split() 
+        group_names = shape.name.split()
         for name in group_names:
             if name not in segm:
                 segm[name] = []
@@ -53,5 +64,3 @@ def load(mesh_path, triangulate=False):
 
     group_map = GroupMap.from_dict(segm, len(all_faces))
     return Mesh(v=tinyobj_vertices, f=all_faces, face_groups=group_map)
-
-
