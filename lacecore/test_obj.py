aafd35858284f0b6a32b3eb4c8d4833989b5aa48
Jacob Beard <jake@minnow.io>
2020-02-21 06:02:34 -0500

    Add assertion

--- lacecore/test_obj.py
+++ lacecore/test_obj.py
@@ -1,10 +1,12 @@
 from ._obj import load, LoadException, ArityException
 from ._group_map import GroupMap
+import numpy as np
 import pytest
 
 def test_loads_from_local_path_using_serializer_success_1():
     m = load("./examples/tinyobjloader/models/cube.obj")
     assert(m.num_v == 8)
+    np.testing.assert_array_equal(m.v[0], np.array([0.0, 2.0, 2.0]))
     assert(m.num_f == 6)
     assert(isinstance(m.face_groups, GroupMap))
     assert m.face_groups.keys() == [
