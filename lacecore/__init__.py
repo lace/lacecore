8f1ef8b848f76103632eebf39fc764ceea0cf8fa
Paul Melnikow <email@paulmelnikow.com>
2020-03-03 14:46:16 -0500

    Add `reindex_vertices()` function

--- lacecore/__init__.py
+++ lacecore/__init__.py
@@ -3,4 +3,5 @@ from ._mesh import Mesh  # noqa: F401
 from ._obj import ArityException, LoadException, load as load_obj  # noqa: F401
 from ._selection.selection_object import Selection  # noqa: F401
 from ._transform.transform_object import Transform  # noqa: F401
+from ._common.reindexing import reindex_vertices  # noqa: F401
 from .package_version import __version__  # noqa: F401
--- lacecore/_common/test_reindexing.py
+++ lacecore/_common/test_reindexing.py
@@ -1,5 +1,10 @@
 import numpy as np
-from .reindexing import indices_of_original_elements_after_applying_mask
+import pytest
+from .reindexing import (
+    indices_of_original_elements_after_applying_mask,
+    reindex_vertices,
+)
+from .. import shapes
 
 
 def test_reindexing_mask():
@@ -24,3 +29,21 @@ def test_reindexing_mask_remove_all():
         indices_of_original_elements_after_applying_mask(np.repeat(False, 12)),
         np.repeat(-1, 12),
     )
+
+
+def test_reindex_vertices():
+    cube = shapes.cube(np.zeros(3), 3.0)
+
+    ordering = np.random.permutation(8)
+    reindexed_cube = reindex_vertices(cube, ordering)
+
+    np.testing.assert_array_equal(reindexed_cube.v, cube.v[ordering])
+    np.testing.assert_array_equal(reindexed_cube.v[reindexed_cube.f], cube.v[cube.f])
+
+
+def test_reindex_vertices_error():
+    with pytest.raises(ValueError, match="Expected new vertex indices to be unique, and range from 0 to 8"):
+        reindex_vertices(
+            shapes.cube(np.zeros(3), 3.0),
+            np.array([0, 0, 1, 2, 3, 4, 5, 6])
+        )
