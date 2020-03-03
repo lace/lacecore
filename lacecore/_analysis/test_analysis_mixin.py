6d7dd5e456077aa27191c543a244482de37681c4
Paul Melnikow <github@paulmelnikow.com>
2020-03-03 11:00:05 -0600

    Add `apex()` method and `bounding_box` property (#17)
    Closes #8
    Closes #10

--- lacecore/_analysis/test_analysis_mixin.py
+++ lacecore/_analysis/test_analysis_mixin.py
@@ -7,3 +7,16 @@ def test_vertex_centroid():
     np.testing.assert_array_almost_equal(
         cube_at_origin.vertex_centroid, np.repeat(1.5, 3)
     )
+
+
+def test_bounding_box():
+    bounding_box = shapes.cube(np.zeros(3), 3.0).bounding_box
+    np.testing.assert_array_equal(bounding_box.origin, np.zeros(3))
+    np.testing.assert_array_equal(bounding_box.size, np.repeat(3, 3))
+
+
+def test_apex():
+    cube_at_origin = shapes.cube(np.zeros(3), 3.0)
+    np.testing.assert_array_almost_equal(
+        cube_at_origin.apex(np.array([1.0, 1.0, -1.0])), np.array([3.0, 3.0, 0.0])
+    )
