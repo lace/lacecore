09e565abb7ed968c5965cedbdd925ba47f765de1
Paul Melnikow <github@paulmelnikow.com>
2026-02-23 22:07:59 -0500

    Replace executor with sh to support Python 3.13 and 3.14 (#167)

--- dev.py
+++ dev.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env -S poetry run python
 
+import glob
 import os
+import shutil
 import click
-from executor import execute
+import sh
 
 
 def python_source_files():
-    import glob
-
     include_paths = (
         glob.glob("*.py")
         + glob.glob("lacecore/*.py")
@@ -25,64 +25,79 @@ def cli():
 
 @cli.command()
 def install():
-    execute(
-        "poetry install --sync --extras doc --extras lint --extras format --extras test --extras cli --extras obj"
+    sh.poetry(
+        "install",
+        "--sync",
+        "--extras",
+        "doc",
+        "--extras",
+        "lint",
+        "--extras",
+        "format",
+        "--extras",
+        "test",
+        "--extras",
+        "cli",
+        "--extras",
+        "obj",
+        _fg=True,
     )
 
 
 @cli.command()
 def test():
-    execute("pytest")
+    sh.pytest(_fg=True)
 
 
 @cli.command()
 def coverage():
-    execute("pytest --cov=lacecore")
+    sh.pytest("--cov=lacecore", _fg=True)
 
 
 @cli.command()
 def coverage_report():
-    execute("coverage html --fail-under=0")
-    execute("open htmlcov/index.html")
+    sh.coverage("html", "--fail-under=0", _fg=True)
+    sh.open("htmlcov/index.html", _fg=True)
 
 
 @cli.command()
 def lint():
-    execute("flake8", *python_source_files())
+    sh.flake8(*python_source_files(), _fg=True)
 
 
 @cli.command()
 def black():
-    execute("black", *python_source_files())
+    sh.black(*python_source_files(), _fg=True)
 
 
 @cli.command()
 def black_check():
-    execute("black", "--check", *python_source_files())
+    sh.black("--check", *python_source_files(), _fg=True)
 
 
 @cli.command()
 def doc():
-    execute("rm -rf build/ doc/build/ doc/api/")
-    execute("sphinx-build -W -b singlehtml doc doc/build")
+    sh.rm("-rf", "build/", "doc/build/", "doc/api/", _fg=True)
+    sh.Command("sphinx-build")("-W", "-b", "singlehtml", "doc", "doc/build", _fg=True)
 
 
 @cli.command()
 def doc_open():
-    execute("open doc/build/index.html")
+    sh.open("doc/build/index.html", _fg=True)
 
 
 @cli.command()
 def clean():
-    execute("find . -name '*.pyc' -delete")
-    execute("find . -name '__pycache__' -delete")
+    sh.find(".", "-name", "*.pyc", "-delete", _fg=True)
+    sh.find(".", "-name", "__pycache__", "-delete", _fg=True)
 
 
 @cli.command()
 def publish():
-    execute("rm -rf dist/ build/")
-    execute("poetry build")
-    execute("twine upload dist/*")
+    shutil.rmtree("dist", ignore_errors=True)
+    shutil.rmtree("build", ignore_errors=True)
+    sh.poetry("build", _fg=True)
+    sh.twine("upload", *glob.glob("dist/*"), _fg=True)
 
 
 if __name__ == "__main__":
